9f567e0f07e79426cbb22469398a4bc5383547dd
Omniverseall <omniverseall4215062000@gmail.com>
2025-07-03 22:03:28 +0500

    Updated LoadingAnimation.tsx

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Logo from './Logo.png';
+import Logo from '../../favicon.ico';
 
 interface LoadingAnimationProps {
   onComplete: () => void;
