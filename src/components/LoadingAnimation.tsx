07f9466a883f2af44554be6c78f689a6d9e93101
Omniverseall <omniverseall4215062000@gmail.com>
2025-07-03 22:01:07 +0500

    Fix Logo import path in LoadingAnimation component

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Logo from '../../Logo.png';
+import Logo from './Logo.png';
 
 interface LoadingAnimationProps {
   onComplete: () => void;
